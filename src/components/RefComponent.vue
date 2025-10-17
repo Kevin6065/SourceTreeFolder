0adebe68926715aa181394e986645466cde41650
Kevin6065 <kevinyang0502@gmail.com>
2025-10-17 19:54:20 +0800

    focusFirstInput

--- src/components/RefComponent.vue
+++ src/components/RefComponent.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const items = ref([
   { id: 1, name: '蘋果', placeholder: '請輸入數量' },
@@ -11,8 +11,9 @@ const items = ref([
 const inputData = ref([])
 
 // 設置輸入框的 ref
-const setInputRef = (index) => (el) => {
-  inputData.value[index] = el
+// 設定每一個input ref
+const setInputRef = (idx, el) => {
+  inputData.value[idx] = el
 }
 
 const focusFirstInput = () => {
@@ -32,13 +33,23 @@ const submit = () => {
     console.log(`${items.value[index].name} 數量 ${item.value}`)
   })
 }
+
+if (inputData.value[0]) {
+  inputData.value[0].focus()
+}
+
+onMounted(() => {
+  if (inputData.value[0]) {
+    inputData.value[0].focus()
+  }
+})
 </script>
 
 <template>
   <div>
     <div v-for="(item, index) in items" :key="item.id">
       <label> {{ item.name }}</label>
-      <input type="text" :ref="setInputRef(index)" :placeholder="item.placeholder" />
+      <input type="text" :ref="(el) => setInputRef(index, el)" :placeholder="item.placeholder" />
     </div>
     <button @click="focusFirstInput">重新輸入(聚焦第一個輸入框)</button>
     <button @click="submit">送出</button>
